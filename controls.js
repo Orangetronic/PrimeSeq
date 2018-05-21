412fab8fee0d0c80e62125bc56102d7b2c854f8f
David Miller <david@orangetronic.com>
2018-05-21 12:48:00 +0100

    volume control, colour unity

--- controls.js
+++ controls.js
@@ -8,10 +8,11 @@ class Controls {
     this.container.appendChild(this.name)
 
     this.controls = [
-      new Slider("Cutoff",    "cutoff",  1, 0.02, 15, 0.01, val => synth.cutoff(val)),
-      new Slider("Resonance", "res",     1, 0.1,  37, 0.1 , val => synth.resonance(val)),
-      new Slider("Gate",      "gate",    2, 0.01, 3,  0.01, val => synth.setGateTime(val)),
-      new Slider("Effects",   "effects", 0, 0,    1,  0.05, val => synth.wetness(val))
+      new Slider("Volume",    "vol",     0.3, 0,    0.45, 0.01, val => synth.volume(val)),
+      new Slider("Cutoff",    "cutoff",  1,   0.02, 15,   0.01, val => synth.cutoff(val)),
+      new Slider("Resonance", "res",     1,   0.1,  37,   0.1 , val => synth.resonance(val)),
+      new Slider("Gate",      "gate",    2,   0.01, 3,    0.01, val => synth.setGateTime(val)),
+      new Slider("Effects",   "effects", 0,   0,    1,    0.05, val => synth.wetness(val))
     ]
 
     this.controls.forEach(control => this.container.appendChild(control.container))
